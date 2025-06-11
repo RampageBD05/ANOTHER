1310a8cdfbf099772ed92f6e76880780c9628b4a
RampageBD05 <113905358+RampageBD05@users.noreply.github.com>
2025-06-11 15:29:35 +0200

    Removed Commented Code

---
+++ scripts.js
@@ -0,0 +1,19 @@
+tsParticles.load("tsparticles", {
+  fullScreen: { enable: false },
+  particles: {
+    number: { value: 80 },
+    color: { value: "#ffffff" },
+    shape: { type: "circle" },
+    opacity: { value: 0.5 },
+    size: { value: 3 },
+    move: { enable: true, speed: 2 }
+  },
+  interactivity: {
+    events: {
+      onHover: { enable: true, mode: "repulse" }
+    },
+    modes: {
+      repulse: { distance: 100 }
+    }
+  }
+});
